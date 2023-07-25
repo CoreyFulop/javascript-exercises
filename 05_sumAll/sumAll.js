48cb9fa7c4023ada711ba8638e7409d74836cc2b
Corey Fulop <corey.fulop@gmail.com>
2023-07-25 16:37:56 +0100

    Complete TDD exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((num1 < 0 || num2 < 0) || (typeof num1 !== "number" || typeof num2 !== "number")) {
+        return "ERROR";
+    }
+    if (num1 < num2) {
+        let lower = num1;
+        let upper = num2;
+        let sum = 0;
+        for (lower; lower <= upper; lower++) {
+            sum += lower;
+        }
+        return sum;
+    } else {
+        let lower = num2;
+        let upper = num1;
+        let sum = 0;
+        for (lower; lower <= upper; lower++) {
+            sum += lower;
+        }
+        return sum;
+    }   
 };
 
 // Do not edit below this line
