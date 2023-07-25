f4b884ebb1be14a350b9e9fd0e4f0cbdca27ccc6
Corey Fulop <corey.fulop@gmail.com>
2023-07-25 12:43:59 +0100

    Complete TDD exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let outString = "";
+    let stringList = [];
+    for (let char of string) {
+        stringList.push(char);
+    }
+    while (stringList.length > 0) {
+        outString += stringList.pop();
+    }
+    return outString;
 };
 
 // Do not edit below this line
