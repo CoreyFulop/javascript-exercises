e5381b0e36582458a6984d8b042ed880a2390670
Corey Fulop <corey.fulop@gmail.com>
2023-07-25 15:31:18 +0100

    Complete TDD exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(principalArray, ...removeArray) {
+    for (let arrayElement of removeArray) {
+        while (principalArray.includes(arrayElement)) {
+            removalIndex = principalArray.indexOf(arrayElement);
+            principalArray.splice(removalIndex, 1);
+        }
+    }
+    return principalArray;
 };
 
 // Do not edit below this line
